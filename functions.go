26b8be7bf0c20376741a20046fbb175fb76fffa2
Ameya Bhurke <ameyabhurke@gmail.com>
2018-04-10 22:01:29 +1000

    for loop

--- functions.go
+++ functions.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const greeting = "hello"
+
 func add(x, y int) int {
 	return x + y
 }
@@ -19,6 +21,12 @@ func main() {
 	fmt.Printf("swap 2 and 3 returns %d and %d", a, b)
 	fmt.Println()
 	var c, python, java = true, false, "Hell no!"
-
 	fmt.Println(c, python, java)
+	fmt.Println(greeting)
+	var sum = 0
+	for i := 0; i < 5; i++ {
+		sum += i
+	}
+	fmt.Printf("The sum is %d", sum)
+	fmt.Println()
 }
