174c8fb4c7c03b3a2b81ec4e6ae74cb388f341d3
Ameya Bhurke <ameyabhurke@gmail.com>
2018-04-19 19:21:03 +1000

    fixed the obvious bug

--- fibonnaci.go
+++ fibonnaci.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 var pre int = 0
-var post int = 0
+var post int = 1
 
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
 	return func() int {
 		pre, post = post, pre+post
-		return post
+		return pre
 	}
 }
 
