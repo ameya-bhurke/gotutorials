5eacfb924fa330850c977008e7551adf2dbfd177
Ameya Bhurke <ameyabhurke@gmail.com>
2018-04-20 22:31:32 +1000

    methods with pointer receivers

--- methods.go
+++ methods.go
@@ -16,9 +16,17 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// Scale scales the vertex by a factor f
+func (v* Vertex) Scale(factor float64) {
+	v.X = v.X * factor
+	v.Y = v.Y * factor
+}
+
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
 	v = Vertex{1, 2}
 	fmt.Println(v.Abs())
+	v.Scale(1.2)
+	fmt.Println(v)
 }
