6dad000274756262d87775fdf564436b4c456b56
Ameya Bhurke <ameyabhurke@gmail.com>
2018-04-22 13:46:50 +1000

    type inference

--- methods.go
+++ methods.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+type Abser interface {
+	Abs() float64
+}
+
 // Vertex A vertex on a 2 dimensional cartesian coordinate plane.
 type Vertex struct {
 	X float64
@@ -17,13 +21,18 @@ func (v Vertex) Abs() float64 {
 }
 
 // Scale scales the vertex by a factor f
-func (v* Vertex) Scale(factor float64) {
+func (v *Vertex) Scale(factor float64) {
 	v.X = v.X * factor
 	v.Y = v.Y * factor
 }
 
 func main() {
 	v := Vertex{3, 4}
+	var a Abser
+	a = &v
+	t, ok := a.(Abser)
+	fmt.Printf("Is a an Abser? %v \n", ok)
+	fmt.Println(t)
 	fmt.Println(v.Abs())
 	v = Vertex{1, 2}
 	fmt.Println(v.Abs())
