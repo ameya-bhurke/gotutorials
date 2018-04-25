0f51acf03fc7259e8214ebeec26cdfb7c461a329
Ameya Bhurke <ameyabhurke@gmail.com>
2018-04-25 11:12:05 +1000

    added error handling for negative numbers

--- sqrt.go
+++ sqrt.go
@@ -5,15 +5,25 @@ import (
 	"math"
 )
 
-func sqrt(x float64) float64 {
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("Cannot square root a negative number: %f", e)
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0.0, ErrNegativeSqrt(x)
+	}
 	var y = float64(1)
 	for math.Abs(x-y*2) > 0.00 {
 		y -= ((y * y) - x) / (2 * y)
 		y = math.Abs(y)
 	}
-	return y
+	return y, nil
 }
 
 func main() {
-	fmt.Printf("The square root of 4 is %f", sqrt(4))
+	fmt.Println(sqrt(2))
+	fmt.Println(sqrt(-2))
 }
